a24ac69f2476fa787c3613111065a758bcbb29fd
Bhavana-anu <anushalalam01@gmail.com>
2024-03-09 14:25:27 +0530

    first commit

---
+++ src/main/java/com/ty/security/qrcode/DemoSmsSenderPhoneApplication.java
@@ -0,0 +1,13 @@
+package com.ty.security.qrcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSmsSenderPhoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSmsSenderPhoneApplication.class, args);
+	}
+
+}
